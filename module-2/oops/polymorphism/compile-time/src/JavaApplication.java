612400e01041435d1d9937c55a05a4d4e736feac
cse-manideep-polusani <cse.manideep.polusani@gmail.com>
2026-01-06 14:54:53 +0530

    Java first commit

--- module-2/oops/polymorphism/compile-time/src/JavaApplication.java
+++ module-2/oops/polymorphism/compile-time/src/JavaApplication.java
@@ -6,5 +6,19 @@ class JavaApplication {
         System.out.println(BasicCalculator.addition(1.9,2.9,3.9));
         System.out.println(BasicCalculator.addition(1,2.9));
         System.out.println(BasicCalculator.addition(1.9,2));
+
+        //Non-static overloading
+        Instagram user1=new Instagram();
+        user1.login("Manideep","Mani@1456");
+
+        Instagram user2=new Instagram();
+        user2.login("Manideep","Mani@1456","A1In7Uj");
+
+        Instagram user3=new Instagram();
+        user3.login("Manideep@gmail.com","1456");
+
+        Instagram user4=new Instagram();
+        user4.login(9989585851l,1456);
+
     }
 }
