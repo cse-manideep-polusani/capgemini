bb89bfd5777ba81a6b731aff339bf5ab7952e60e
cse-manideep-polusani <cse.manideep.polusani@gmail.com>
2026-01-06 15:07:13 +0530

    Java first commit

--- module-2/oops/polymorphism/run-time/src/Father.java
+++ module-2/oops/polymorphism/run-time/src/Father.java
@@ -1,14 +1,18 @@
 class Father {
+
     public void carrer(){
         System.out.println("Future Engineer");
     }
 
-    //Final method cant be overridden ,if we try to Override it give CTE.able to inherit but cant change it.
-    //Static methods cant be overridden,it is called overhiding.
+    // Final methods CANNOT be overridden.
+    // They can be inherited by the child class, but their implementation cannot be changed.
+    // If we try to override a final method, the compiler gives a Compile-Time Error (CTE).
     public final void marriage(){
         System.out.println("Arranged Marriage");
     }
 
+    // This method returns a BasicCalculator object.
+    // It can be overridden in the child class using a covariant return type.
     public BasicCalculator buyCalculator(){
         return new BasicCalculator();
     }
--- module-2/oops/polymorphism/run-time/src/Son.java
+++ module-2/oops/polymorphism/run-time/src/Son.java
@@ -1,19 +1,25 @@
 class Son extends Father {
-    //@Override Act like alarm and give CTE if we mismatched method name in inheritance.
+
+    // @Override acts like a safety check.
+    // It gives a Compile-Time Error (CTE) if the method name does not exactly match the parent class method.
     @Override
     public void carrer(){
         System.out.println("Future Doctor");
     }
 
+    // Cannot override marriage() because it is declared as final in the Father class.
+    // Final methods can be inherited but cannot be overridden.
+    // @Override here would cause a Compile-Time Error.
 //    @Override
 //    public void marriage(){
 //        System.out.println("Love Marriage");
 //    }
 
-    //Variance--->Covariant return type.
-    //we can change return type of method by using Covariant.
-    //Only applicable for Non-Primitive Data Types.
-    //While overriding method should have equal or higher accessibility.
+    // Covariant return type:
+    // While overriding a method, the child class can change the return type
+    // to a subclass of the parent method’s return type.
+    // Applicable only for non-primitive data types.
+    // The access level of the overridden method must be the same or higher.
     @Override
     public ScientificCalculator buyCalculator(){
         return new ScientificCalculator();
