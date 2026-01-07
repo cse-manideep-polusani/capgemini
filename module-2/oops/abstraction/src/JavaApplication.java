126c5cc4650f219207e133567f526801d48eef9a
cse-manideep-polusani <cse.manideep.polusani@gmail.com>
2026-01-07 12:22:16 +0530

    Java first commit

---
+++ module-2/oops/abstraction/src/JavaApplication.java
@@ -0,0 +1,9 @@
+public class JavaApplication {
+    public static void execution(){
+        Punjab punjab = new Punjab();
+        punjab.regionalLanguage();
+
+        Telangana telangana = new Telangana();
+        telangana.regionalLanguage();
+    }
+}
---
+++ module-2/oops/abstraction/src/Telangana.java
@@ -0,0 +1,6 @@
+class Telangana extends State {
+    @Override
+    public void regionalLanguage(){
+        System.out.println("Telugu is the Regional language of Telangana state.");
+    }
+}
