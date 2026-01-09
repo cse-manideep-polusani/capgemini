9e39a61267e2e061aa8307dfb90b7d05dac44e53
cse-manideep-polusani <cse.manideep.polusani@gmail.com>
2026-01-09 12:41:36 +0530

    Java first commit

--- module-2/typecasting/homogenous/primitive-primitive/src/JavaApplication.java
+++ module-2/typecasting/homogenous/primitive-primitive/src/JavaApplication.java
@@ -1,6 +1,8 @@
 class JavaApplication {
     public static void execution(){
-        TypeCasting.widening();
-        TypeCasting.narrowing();
+//        TypeCasting.widening();
+//        TypeCasting.narrowing();
+        System.out.println(TypeCasting.widening('z'));
+        System.out.println(TypeCasting.narrowing(87));
     }
 }
--- module-2/typecasting/homogenous/primitive-primitive/src/TypeCasting.java
+++ module-2/typecasting/homogenous/primitive-primitive/src/TypeCasting.java
@@ -1,17 +1,37 @@
 class TypeCasting {
-    public static void  widening(){
-        int i=10;
-        System.out.println("i is : "+i);
-        double d=i;
-        System.out.println("d is : "+d);
-        System.out.println();
+//    public static void  widening(){
+//        int i=10;
+//        System.out.println("i is : "+i);
+//        double d=i;
+//        System.out.println("d is : "+d);
+//        System.out.println();
+//
+//        char c = 'A';
+//        System.out.println("c is : "+c);
+//        int i1 = c;
+//        System.out.println("i is : "+i1);
+//        System.out.println();
+//    }
+
+    public static int widening(char c){
+        return c;
     }
 
-    public static void narrowing(){
-        double d=10.98;
-        System.out.println("d is : "+d);
-        int i =(int)d;
-        System.out.println("i is : "+i);
-        System.out.println();
+//    public static void narrowing(){
+//        double d=10.98;
+//        System.out.println("d is : "+d);
+//        int i =(int)d;
+//        System.out.println("i is : "+i);
+//        System.out.println();
+//
+//        int i1 = 122;
+//        System.out.println("i is : "+i1);
+//        char c = (char)i1;
+//        System.out.println("c is : "+c);
+//        System.out.println();
+//    }
+
+    public static char narrowing(int i){
+        return (char)i;
     }
 }
