bf7d20a3652be663f3041651d64c92a49622e0df
cse-manideep-polusani <cse.manideep.polusani@gmail.com>
2026-01-10 10:12:56 +0530

    Java first commit

---
+++ module-2/typecasting/heterogenous/src/JavaApplication.java
@@ -0,0 +1,11 @@
+import java.lang.reflect.Type;
+
+class JavaApplication {
+    public static void execution(){
+        TypeCasting.boxing();
+        TypeCasting.autoboxing();
+
+        TypeCasting.unboxing();
+        TypeCasting.autounboxing();
+    }
+}
---
+++ module-2/typecasting/heterogenous/src/TypeCasting.java
@@ -0,0 +1,33 @@
+class TypeCasting{
+    public static void boxing(){
+        int i=10;
+        System.out.println("i is : "+i);
+        Integer I = Integer.valueOf(i);//boxing
+        System.out.println("I is : "+I);
+        System.out.println();
+    }
+
+    public static void autoboxing(){
+        int j=20;
+        System.out.println("j is : "+j);
+        Integer I = j;//autoboxing
+        System.out.println("I is : "+I);
+        System.out.println();
+    }
+
+    public static void unboxing(){
+        Double D = Double.valueOf(12.34);
+        System.out.println("D is : "+D);
+        double d = D.doubleValue();//unboxing
+        System.out.println("d is : "+d);
+        System.out.println();
+    }
+
+    public static void autounboxing(){
+        Double D = Double.valueOf(12.34);
+        System.out.println("D is : "+D);
+        double d = D;//auto-unboxing
+        System.out.println("d is : "+d);
+        System.out.println();
+    }
+}
